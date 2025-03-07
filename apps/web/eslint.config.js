347bd0704561a27c5f385f12701ec6d1c395bf05
FusiDaniel <fpires.daniel@gmail.com>
2025-03-07 11:23:11 -0300

    chore: reconfig eslint

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,5 +1,4 @@
-/* eslint-disable import/no-default-export */
-import { nextJsConfig } from '@repo/eslint-config/next-js';
+import { nextJsConfig } from "@repo/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,21 +1,14 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
-import { config as baseConfig } from "./base.js";
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import { config as baseConfig } from './base.js';
 
 /**
  * A custom ESLint configuration for libraries that use React.
  *
- * @type {import("eslint").Linter.Config} */
+  @type {import("eslint").Linter.Config[]} */
 export const config = [
   ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
@@ -27,13 +20,13 @@ export const config = [
   },
   {
     plugins: {
-      "react-hooks": pluginReactHooks,
+      'react-hooks': pluginReactHooks,
     },
-    settings: { react: { version: "detect" } },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
       // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
+      'react/react-in-jsx-scope': 'off',
     },
+    settings: { react: { version: 'detect' } },
   },
 ];
